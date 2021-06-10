6958ce835aa45176b6a125e4000318a834468cf7
surebits <breadphace@gmail.com>
2021-06-10 19:59:24 +0100

    nit debug

--- word.js
+++ word.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql2');
 
 const conn = mysql.createConnection({
-  host: "localhost",
+//   host: "localhost",
 //   host: "https://cs472-dict.herokuapp.com",
   user: "root",
   password: "password",
